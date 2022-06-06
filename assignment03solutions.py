6f65155f51ecbf77ebe3f885dd2a22c392c99871
sumitkaran6 <104153979+sumitkaran6@users.noreply.github.com>
2022-06-06 10:21:43 +0530

    Add files via upload

---
+++ assignment03solutions.py
@@ -0,0 +1,34 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/Sumit Karan/AppData/Local/Programs/Python/Python310/assignment03.py
+Enter a number to convert it into its binary equivalent:98
+ Its Binary value:
+01100010Select an operation to perform : 
+1. ADD
+2. SUBTRACT
+3. MULTIPLY
+4. DIVIDE
+2
+Enter first number: 60
+Enter second number: 25
+The difference is :35
+Enter the value of a :3
+Enter the value of b :4
+Enter the value of c :5
+Ans = 35
+Enter the value of n :5
+Ans = 10.0
+Ans = 4*math.pi*r**2
+Ans = math.sqrt(r*(math.cos(a))*2+r(math.sin(a))**2
+Ans = y2-y1)/(x2-x1
+0 1 2 3 4 
+3 4 5 6 7 8 9 
+4 7 10 
+15 13 11 9 7 
+7
+This program finds the total molecular weight of a combination of hydrogen, carbon and oxygen atoms :
+How many hydrogen atoms? 3
+Carbon? 1
+Oxygen? 1
+The total molecular weight is 31.033920000000002
