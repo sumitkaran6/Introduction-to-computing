de9983ffeadfc5839e8414fae3f633cc8a65d919
sumitkaran6 <104153979+sumitkaran6@users.noreply.github.com>
2022-06-13 09:12:00 +0530

    Update assignment03.py

--- assignment03.py
+++ assignment03.py
@@ -96,8 +96,8 @@ print(i)
 
 #Q.5
 print("This program finds the total molecular weight of a combination of hydrogen, carbon and oxygen atoms :")
-h = eval(input("How many hydrogen atoms? "))
-c = eval(input("Carbon? "))
-o = eval(input("Oxygen? "))
+h = int(input("How many hydrogen atoms? "))
+c = int(input("Carbon? "))
+o = int(input("Oxygen? "))
 total = (h * 1.00794) + (c * 12.0107) + (o * 15.9994)
 print("The total molecular weight is", total)
