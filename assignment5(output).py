046d456637f16358e4f1a7ea210635a176d213fc
sumitkaran6 <104153979+sumitkaran6@users.noreply.github.com>
2022-06-19 10:46:39 +0530

    Add files via upload

---
+++ assignment5(output).py
@@ -0,0 +1,101 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======== RESTART: C:/Users/Sumit Karan/OneDrive/Documents/assignment5.py =======
+ANS.1
+Enter the the string :tiger
+regit
+ANS.2
+Enter the minimum value :4
+Enter the maximum value: 80
+Enter the number for divisibility check :5
+5
+10
+15
+20
+25
+30
+35
+40
+45
+50
+55
+60
+65
+70
+75
+80
+ANS.3
+Enter the length of side:25
+Enter the length of side:62
+Enter the length of side:46
+504.56608833729604
+ANS.4
+Enter the no. of rows :5
+*
+**
+***
+**
+*
+ANS.5
+Enter the number of rows6
+
+A
+BC
+DEF
+GHIJ
+KLMNO
+PQRSTU
+ANS.6
+Enter the Lowest Range Value : 2
+Enter the Upper Range Value : 80
+The Prime Numbers in this range are : 
+2
+3
+5
+7
+11
+13
+17
+19
+23
+29
+31
+37
+41
+43
+47
+53
+59
+61
+67
+71
+73
+79
+ANS.7
+The numbers divisible by 11 and are multiple of 7 are : [77, 154, 231, 308, 385, 462]
+ANS.8
+Enter the number 52
+Enter the number 45
+Enter the number -5
+Enter the number -89
+Enter the number 58
+Enter the number -89
+Enter the number -41
+Enter the number 54
+Enter the number -1
+Enter the number 25
+Positive numbers are:  [52, 45, 58, 54, 25]
+Negative numbers are:  [-5, -89, -89, -41, -1]
+Even numbers are:  [52, 58, 54]
+Odd numbers are:  [45, -5, -89, -89, -41, -1, 25]
+Number of times each number occurs in the List {52: 1, 45: 1, -5: 1, -89: 2, 58: 1, -41: 1, 54: 1, -1: 1, 25: 1}
+ANS.9
+Number of words: 6
+Enter the word: hello
+Enter the word: bye
+Enter the word: nice
+Enter the word: good
+Enter the word: bad
+Enter the word: nice
+Number of occurences:  {'hello': 1, 'bye': 1, 'nice': 2, 'good': 1, 'bad': 1}
