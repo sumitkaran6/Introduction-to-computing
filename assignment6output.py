43ad5c87baea0dc8d37235a6047f15d2962e83ea
sumitkaran6 <104153979+sumitkaran6@users.noreply.github.com>
2022-07-03 20:25:19 +0530

    Add files via upload

---
+++ assignment6output.py
@@ -0,0 +1,47 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======== RESTART: C:/Users/Sumit Karan/OneDrive/Documents/assignment6.py =======
+###################### Question 1 #####################
+Enter a number to find if it is a perfect number or not : 21
+The number is not a perfect number.
+###################### Question 2 #####################
+Enter a word to check whether it is palindrome or not : naman
+The word is palindrome.
+###################### Question 3 #####################
+Enter the number of rows : 6
+                     [1]
+                   [1, 1]
+                 [1, 2, 1]
+               [1, 3, 3, 1]
+             [1, 4, 6, 4, 1]
+           [1, 5, 10, 10, 5, 1]
+###################### Question 4 #####################
+Enter the sentence to check whether it is panagram or not: The quick brown fox jumps over the lazy dog
+The sentence is panagram.
+###################### Question 5 #####################
+Enter the string with hyphen separated sequence to arrange it in alphabetical order : green-red-yellow-black-white
+black -green -red -white -yellow - 
+###################### Question 6 #####################
+Enter the Student ID : 21102123
+If you want to enter only student name then press n, only student class then c or both then b : b
+Enter the Student's Name : sumit karan singh
+Enter the Student's class : semester 2
+### Data Print ###
+Student ID :  21102123
+Student Name :  sumit karan singh
+Student Class :  semester 2
+###################### Question 7 #####################
+The instance student_1 is a instance of class student :  True
+The instance student_1 is a instance of class Marks :  False
+The instance Marks_1 is a instance of class student :  False
+The instance Marks_1 is a instance of class Marks :  True
+The class student is a sub-class of of the built in object class :  True
+The class Marks is a sub-class of of the built in object class :  True
+###################### Question 8 #####################
+Enter the numbers : -25, -10, -7, -3, 2, 4, 8, 10
+[-25, -10, -7, -3, 2, 4, 8, 10]
+The numbers which makes the sum equal to 0 are :  [[-10, 2, 8], [-7, -3, 10]]
+###################### Question 9 #####################
+Enter the set of parenthesis to check whether it is valid or not : ()
+The set of parenthesis is  valid.
